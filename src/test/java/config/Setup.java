c1ebe3ef2f379e80ae496a2f1a99a56ffd99328b
Azharorin <azharalamorin@gmail.com>
2025-11-06 17:49:31 +0600

    adding testNG suites

--- src/test/java/config/Setup.java
+++ src/test/java/config/Setup.java
@@ -3,6 +3,7 @@ package config;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -16,7 +17,8 @@ public class Setup {
     @BeforeTest
     public void setUp() {
         //WebDriverManager.chromedriver().setup();
-        driver = new FirefoxDriver();
+        // driver = new FirefoxDriver();
+        driver = new EdgeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         //driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(40));
