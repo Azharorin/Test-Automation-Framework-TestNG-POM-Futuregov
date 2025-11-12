506e90fb109758577ef4a087a907c85e247e8082
Azharorin <azharalamorin@gmail.com>
2025-11-12 12:44:54 +0600

    adding headless browser options

--- src/test/java/config/Setup.java
+++ src/test/java/config/Setup.java
@@ -2,8 +2,11 @@ package config;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
@@ -23,24 +26,49 @@ public class Setup {
         Properties prop = new Properties();
         FileInputStream fis = new FileInputStream("C:\\Users\\azhar.alam\\IdeaProjects\\testng-automation-framework-futuregov\\src\\test\\resources\\GlobalData.properties");
         prop.load(fis);
+
         String browserName = prop.getProperty("browser");
+        boolean headless = Boolean.parseBoolean(prop.getProperty("headless", "false"));
 
         if (browserName.equalsIgnoreCase("chrome")) {
-            driver = new ChromeDriver();
+            ChromeOptions options = new ChromeOptions();
+            if (headless) {
+                options.addArguments("--headless");
+                options.addArguments("--window-size=1920,1080");
+            }
+            driver = new ChromeDriver(options);
+
         } else if (browserName.equalsIgnoreCase("firefox")) {
-            driver = new FirefoxDriver();
+            FirefoxOptions options = new FirefoxOptions();
+            if (headless) {
+                options.addArguments("--headless");
+                options.addArguments("--width=1920");
+                options.addArguments("--height=1080");
+            }
+            driver = new FirefoxDriver(options);
+
         } else if (browserName.equalsIgnoreCase("edge")) {
-            driver = new EdgeDriver();
+            EdgeOptions options = new EdgeOptions();
+            if (headless) {
+                options.addArguments("--headless");
+                options.addArguments("--window-size=1920,1080");
+            }
+            driver = new EdgeDriver(options);
+
+        } else {
+            throw new IllegalArgumentException("Unsupported browser: " + browserName);
         }
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         return driver;
     }
 
+
     @BeforeMethod
 
     public LoginPage launchApplication() throws IOException {
-        driver = setUp();
+        //driver = setUp();
         loginPage = new LoginPage(driver);
         loginPage.goTo();
 
